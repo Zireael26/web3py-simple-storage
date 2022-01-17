e00eee6693354a1065b477f6f954a23a666d28ca
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2022-01-17 06:31:10 +0530

    Switched to Rinkeby + Infura

--- deploy.py
+++ deploy.py
@@ -36,9 +36,12 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 #  Connecting to ganache
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-chain_id = 1337
-my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+# w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+w3 = Web3(
+    Web3.HTTPProvider("https://rinkeby.infura.io/v3/b28a4ca8f39649ebbdfa6ff609765909")
+)
+chain_id = 4
+my_address = "0xde7480E66AFF1E32EA141ed15cb88aF1fB98E0Cb"
 private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in python
